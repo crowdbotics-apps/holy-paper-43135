b4a5d64cd61f3f68af5601c7acffa4b79df57cda
Crowdbotics <team@crowdbotics.com>
2023-08-29 12:32:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-paper-43135",
+  url: "https://holy-paper-43135.botics.co",
+  api: "https://holy-paper-43135.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
